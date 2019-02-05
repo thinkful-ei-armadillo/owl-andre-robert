fe513581a4ac30ea0e64749572d8a5293a9c9860
Andre K W <akw.ndre@gmail.com>
2019-02-05 12:47:32 -0500

    created ParticipantList; started foundation

---
+++ src/ParticipantList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+//import './ParticipantList.css';
+//import Participant from './Participant';
+
+class ParticipantList extends Component {
+  render() {
+    return (
+      <section className="participant-list">
+        {/* {this.props.participants.map((p, index) => <Participant key={index} />)} */}
+      </section>
+    );
+  }
+}
+
+export default ParticipantList;
